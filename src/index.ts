43dc483f103f22b33f5f3c914d4568c6801b9eff
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-02-18 11:55:55 -0500

    init: src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const hi: string = "hi";
-process.stdout.write(hi);
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const rl = readline.createInterface({ input, output });
+
+const answer = await rl.question("Question");
+
+console.log(`Thank you for your valuable feedback: ${answer}`);
+
+rl.on("SIGINT", () => {
+  void rl.question("Are you sure you want to exit? ").then((answer) => {
+    if (/^y(es)?$/i.exec(answer)) rl.close();
+  });
+});
