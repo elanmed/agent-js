f31202042c0b0da74d85f3e9ba5a941ba540ec65
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-06-07 22:50:57 -0400

    add: accept cmd arg

--- scripts/paste-server.ts
+++ scripts/paste-server.ts
@@ -1,7 +1,13 @@
 import net from "node:net";
 import { execSync } from "node:child_process";
 
-const server = net.createServer((socket) => socket.end(execSync("pbpaste")));
+const args = process.argv.slice(2);
+
+if (args.length !== 1) {
+  throw new Error("usage: --paste-cmd [cmd]");
+}
+
+const server = net.createServer((socket) => socket.end(execSync(args[1]!)));
 
 server.listen(0, "0.0.0.0", () => {
   const address = server.address();
